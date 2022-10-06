9f09646b62fff6ce97932079a434fd1a2fe93f22
jonathan peña <gran_200014@hotmail.com>
2022-10-06 11:06:09 -0400

    pequenos cambios esteticos

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  globDirectory: "dist/",
-  globPatterns: ["**/*.{svg,webp,png,js,css,html,json}"],
-  swDest: "dist/sw.js",
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+	globDirectory: 'dist/',
+	globPatterns: ['**/*.{svg,webp,png,js,css,html,json}'],
+	swDest: 'dist/assets/sw.js',
+	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
 
 // $ npx workbox generateSW workbox-config.js

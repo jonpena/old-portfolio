1f0284dac1d6eebec4bd8ac9232ec83482076539
jonpena <gran_200014@hotmail.com>
2022-10-23 21:12:27 -0400

    pequenos cambios

--- src/js/multiLanguage.js
+++ src/js/multiLanguage.js
@@ -4,12 +4,10 @@ const changePageLanguage = function (key) {
 
 export function setPageLanguage() {
   let key = window.localStorage.getItem("lang");
-
   const browserLang = (navigator.language || navigator.userLanguage).slice(
     0,
     2
   );
-
   if (key === null) changePageLanguage(browserLang);
 }
 

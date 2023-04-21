d0d581b955183e67163ec8d984e14dd2f33319ec
jonpena <gran_200014@hotmail.com>
2023-04-21 11:55:32 -0400

    adding more skills and a few fixes

--- src/js/tagCloud.js
+++ src/js/tagCloud.js
@@ -1,14 +1,14 @@
 const createTagCloud = (options) => {
-	window.TagCloud('.sphere-container', Array(11).fill(''), options);
-	document.querySelector('.tagcloud').style.width = '100%';
-	document.querySelector('.tagcloud').style.height = '470px';
+  window.TagCloud('.sphere-container', Array(16).fill(''), options);
+  document.querySelector('.tagcloud').style.width = '100%';
+  document.querySelector('.tagcloud').style.height = '470px';
 };
 
 const updateTagCloud = (mq, options) => {
-	const $sphere = document.querySelector('.sphere-container');
-	options.radius = mq.matches ? 360 : 230;
-	$sphere.removeChild($sphere.childNodes[0]);
-	createTagCloud(options);
+  const $sphere = document.querySelector('.sphere-container');
+  options.radius = mq.matches ? 360 : 230;
+  $sphere.removeChild($sphere.childNodes[0]);
+  createTagCloud(options);
 };
 
 export { createTagCloud, updateTagCloud };

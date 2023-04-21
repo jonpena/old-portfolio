1819db553c6d2bff0e191fba411d354d0475edb4
jonpena <gran_200014@hotmail.com>
2023-04-21 12:31:13 -0400

    Updates

--- assets/status.087a0ed5.js
+++
@@ -1,1 +0,0 @@
-import"./components.12fdf15c.js";const o=(t,e)=>{(async()=>{const a=await(await fetch(`./language/${t}.json`)).json();for(const n of e)n.innerHTML=a[n.dataset.section][n.dataset.value]})()};window.addEventListener("DOMContentLoaded",()=>{const t=window.localStorage.getItem("lang");let e=document.querySelectorAll("[data-section]");o(t,e)});
---
+++ assets/status.649f7bc7.js
@@ -0,0 +1,1 @@
+import"./components.22a53017.js";const o=(t,e)=>{(async()=>{const a=await(await fetch(`./language/${t}.json`)).json();for(const n of e)n.innerHTML=a[n.dataset.section][n.dataset.value]})()};window.addEventListener("DOMContentLoaded",()=>{const t=window.localStorage.getItem("lang");let e=document.querySelectorAll("[data-section]");o(t,e)});

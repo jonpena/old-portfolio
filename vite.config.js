29865e9c47e9fbee51adb60ed4e634cd82d51b4a
jonpena <gran_200014@hotmail.com>
2023-05-13 17:29:38 -0400

    cambiando a old portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			input: {
-				index: './index.html',
-				status: './404.html',
-			},
-		},
-	},
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.html',
+        status: './404.html',
+      },
+    },
+  },
 });
